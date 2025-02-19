dc96c8e14ad53e2e04722a488e706f847cd0ee3d
ChreaTongsure <157035379+Tongsure@users.noreply.github.com>
2025-02-19 22:58:40 +0700

    Update cateController.java

--- src/main/java/com/example/demo/controllers/cateController.java
+++ src/main/java/com/example/demo/controllers/cateController.java
@@ -17,7 +17,7 @@ import java.util.List;
 @RequestMapping("v1/test")
 public class cateController {
     @Autowired
-    private cateImplService cateImplService;  
+    private cateImplService cateImplService;       
     @GetMapping("/testdata")
     public List<reponseDto> findCust(@RequestParam  String custId) {
         return  cateImplService.findCust(custId);
